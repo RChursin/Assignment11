5107fdf4ae47ba9bec953de9c3ae96f1bd21941c
ganstacoder <ganstacoder@gmail.com>
2024-05-19 22:00:04 -0700

    Part Math

--- js/math.js
+++ js/math.js
@@ -1,9 +1,21 @@
 //STEP 1
+let number = prompt("Enter a number:");
+console.log(Math.abs(number));
 
 //STEP 2
+// let number = prompt("Enter a floating-point number:");
+// console.log(Math.ceil(number));
 
 //STEP 3
+// let number = prompt("Enter a floating-point number:");
+// console.log(Math.floor(number));
 
 //STEP 4
+// let numbers = prompt("Enter 5 numbers separated by commas:");
+// let numberArray = numbers.split(",");
+// console.log("Largest number: " + Math.max(...numberArray));
+// console.log("Smallest number: " + Math.min(...numberArray));
 
 //STEP 5
+// let number = prompt("Enter a number:");
+// console.log(Math.sqrt(number));
