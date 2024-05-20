74189d7aba7f45b3086682e1c5a2d3be221ef0d6
ganstacoder <ganstacoder@gmail.com>
2024-05-19 21:57:02 -0700

    Part Strings

--- js/strings.js
+++ js/strings.js
@@ -1,19 +1,46 @@
 //STEP 1
+let name = prompt("Please enter your name:");
+alert("The length of your name is: " + name.length);
 
 //STEP 2
+// let name = prompt("Please enter your name:");
+// let index = parseInt(prompt("Enter a numeric value:"));
+// alert("The character at index " + index + " is: " + name.charAt(index));
 
 //STEP 3
+// let firstName = prompt("Please enter your first name:");
+// let lastName = prompt("Please enter your last name:");
+// alert("Your name is: " + firstName + " " + lastName);
 
 //STEP 4
+// let sentence = "The quick brown fox jumps over the lazy dog";
+// alert("The index of the word 'fox' is: " + sentence.indexOf("fox"));
 
 //STEP 5
+// let sentence = "The quick brown fox jumps over the lazy fox";
+// alert("The index of the last instance of the word 'fox' is: " + sentence.lastIndexOf("fox"));
 
 //STEP 6
+// let sentence = "The quick brown fox jumped over the lazy dog";
+// let name = prompt("Please enter your full name:");
+// let newSentence = sentence.replace("the lazy dog", name);
+// alert(newSentence);
 
 //STEP 7
+// let sentence = "The quick brown fox jumps over the lazy dog";
+// let word = prompt("Enter a word to search for:");
+// alert("The word '" + word + "' " + (sentence.includes(word) ? "is" : "is not") + " in the sentence.");
 
 //STEP 8
+// let old_string = "The quick brown fox jumps over the lazy dog";
+// let new_string = old_string.slice(old_string.indexOf("the lazy dog"), old_string.length).toUpperCase();
+// alert(new_string);
 
 //STEP 9
+// let sentence = "  THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ";
+// alert(sentence.trim().toLowerCase());
 
 //STEP 10
+// let sentence = "the quick brown fox jumps over the lazy dog";
+// let capitalizedSentence = sentence.charAt(0).toUpperCase() + sentence.slice(1);
+// alert(capitalizedSentence);
